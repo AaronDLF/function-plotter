9068cfa2c4277febe11cfb73768078b62e397076
AaronDLF <aaron10lopfortich@gmail.com>
2026-01-15 14:37:58 -0500

    Add the quit event type to the program

--- plot.c
+++ plot.c
@@ -1,5 +1,6 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_video.h>
+#include <stdbool.h>
 #include <stdio.h>
 
 #define WIDTH 900
@@ -13,10 +14,14 @@ int main() {
   SDL_Delay(3000);
 
   SDL_Event event;
+  bool app_running = true;
 
-  while (SDL_PollEvent(&event)) {
-    if (event.type == SDL_QUIT) {
-      break;
+  while (app_running) {
+
+    while (SDL_PollEvent(&event)) {
+      if (event.type == SDL_QUIT) {
+        app_running = false;
+      }
     }
   }
 }
