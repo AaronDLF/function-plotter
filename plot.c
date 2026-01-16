1005ade5210e7f7c15b42c7517831a0024436ec4
AaronDLF <aaron10lopfortich@gmail.com>
2026-01-16 13:29:57 -0500

    Add the grid drawing

--- plot.c
+++ plot.c
@@ -8,6 +8,17 @@
 #define HEIGHT 600
 #define COLOR 0xFF0000
 
+void draw_grid(SDL_Surface *psurface) {}
+
+void draw_at_grid_coordinates(SDL_Surface *psurface, SDL_Rect *prect,
+                              Uint32 color) {
+
+  int x_grid = WIDTH / 2 + prect->x;
+  int y_grid = HEIGHT / 2 - prect->y;
+  SDL_Rect *shifted_rect = &(SDL_Rect){x_grid, y_grid, prect->w, prect->h};
+  SDL_FillRect(psurface, shifted_rect, color);
+}
+
 int main() {
   printf("Welcome to the function plotter!\n");
   SDL_Window *pwindow =
@@ -17,7 +28,9 @@ int main() {
   SDL_Surface *psurface = SDL_GetWindowSurface(pwindow);
 
   SDL_Rect rect = (SDL_Rect){50, 50, 50, 50};
-  SDL_FillRect(psurface, &rect, COLOR);
+
+  draw_at_grid_coordinates(psurface, &rect, COLOR);
+  // draw_grid(psurface);
 
   SDL_Event event;
   bool app_running = true;
